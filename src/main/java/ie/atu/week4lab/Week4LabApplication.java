e3bff0c3696fea5d846e8c72921cde5d199f933d
Jakub Mamala <G00408184@atu.ie>
2023-10-17 15:32:44 +0100

    made a application class

---
+++ src/main/java/ie/atu/week4lab/Week4LabApplication.java
@@ -0,0 +1,13 @@
+package ie.atu.week4lab;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Week4LabApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Week4LabApplication.class, args);
+    }
+
+}
