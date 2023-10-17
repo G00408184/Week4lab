3eea37748382dfef3bdb275dd887252f72f375d9
Jakub Mamala <G00408184@atu.ie>
2023-10-17 15:46:13 +0100

    made the post mapping link

--- src/main/java/ie/atu/week4lab/RegistrarionServiceClient.java
+++ src/main/java/ie/atu/week4lab/RegistrarionServiceClient.java
@@ -1,7 +1,11 @@
 package ie.atu.week4lab;
 
 import org.springframework.cloud.openfeign.FeignClient;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 
-@FeignClient()
+@FeignClient(name = "confirmation-service", url = "http://localhost:8081")
 public interface RegistrarionServiceClient {
+    @PostMapping("/confirm")
+    String someDetails(@RequestBody UserDetails userdetails)
 }
