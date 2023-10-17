e144d1145d0e30ad8fadc68e030c9dd75f22d867
Jakub Mamala <G00408184@atu.ie>
2023-10-17 15:45:19 +0100

    created constructors for details

--- src/main/java/ie/atu/week4lab/UserDetails.java
+++ src/main/java/ie/atu/week4lab/UserDetails.java
@@ -1,4 +1,13 @@
 package ie.atu.week4lab;
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
 public class UserDetails {
+    private String name;
+    private String email;
 }
