28526d42a108e0cb74d7c5c8754d91516c76c7d1
Jakub Mamala <G00408184@atu.ie>
2023-10-17 16:22:32 +0100

    Added the @Service annotation to the class

--- src/main/java/ie/atu/week4lab/RegistrarionServiceClient.java
+++ src/main/java/ie/atu/week4lab/RegistrarionServiceClient.java
@@ -1,11 +1,12 @@
 package ie.atu.week4lab;
 
 import org.springframework.cloud.openfeign.FeignClient;
+import org.springframework.stereotype.Service;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
-
+@Service
 @FeignClient(name = "confirmation-service", url = "http://localhost:8081")
 public interface RegistrarionServiceClient {
     @PostMapping("/confirm")
-    String someDetails(@RequestBody UserDetails userdetails)
+    String someDetails(@RequestBody UserDetails userdetails);
 }
